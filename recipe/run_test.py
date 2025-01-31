a537f6b6ec00625f7a7f881c7c10ee48758ef8c6
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-01-31 09:29:13 -0600

    use psutil to clean up processes

---
+++ recipe/run_test.py
@@ -0,0 +1,33 @@
+import sys
+import psutil
+
+PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long"]
+
+SKIPS = [
+    "test_reject(test_rsgi and test_body_stream_req)",
+    "test_scope and workers",
+]
+
+SKIP_OR = " or ".join(SKIPS)
+K = ["-k", f"not ({SKIP_OR})"]
+
+
+def main():
+    proc = psutil.Process([*PYTEST])
+    rc = proc.wait()
+    all_procs = [*proc.children(recursive=True), proc]
+    killed = []
+    for p in all_procs:
+        print(p, end="")
+        if not p.is_running():
+            print("... was not running")
+            continue
+        killed += [p]
+        print("... killed")
+    if killed:
+        psutil.wait_procs(killed)
+    return rc
+
+
+if __name__ == "__main__":
+    sys.exit(main())
