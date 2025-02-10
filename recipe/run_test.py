ac26c687bbfceacc73b65379fce07d6e5ab77059
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-02-10 17:41:09 -0600

    try more test cleanup for windows

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,5 +1,8 @@
 import sys
 import psutil
+import tempfile
+import shutil
+from pathlib import Path
 
 
 SKIPS = [
@@ -13,21 +16,26 @@ K = ["-k", f"not ({SKIP_OR})"]
 
 PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long", *K]
 
+
 def main():
     print(">>>", *PYTEST, flush=True)
-    proc = psutil.Popen([*PYTEST])
+    tmp = Path(tempfile.TemporaryDirectory())
+    shutil.copytree(Path("tests"), tmp / "tests")
+    proc = psutil.Popen([*PYTEST], cwd=str(tmp))
     rc = proc.wait()
     all_procs = [*proc.children(recursive=True), proc]
     killed = []
     for p in all_procs:
         print(p, end="")
         if not p.is_running():
-            print("... was not running")
+            print("... was not running", flush=True)
             continue
         killed += [p]
-        print("... killed")
+        print("... killed", flush=True)
     if killed:
         psutil.wait_procs(killed)
+    print("cleaning...", tmp, flush=True)
+    shutil.rmtree(tmp, ignore_errors=True)
     return rc
 
 
