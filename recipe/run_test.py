76b45b9e1b9964893cf1bfda31a131847b16dedc
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-02-11 07:22:23 -0600

    skip tests on windows

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,11 +1,5 @@
 import sys
-import psutil
-import time
-import shutil
-import tempfile
-import os
-from pathlib import Path
-
+from subprocess import call
 
 SKIPS = [
     "(test_ws and test_reject)",
@@ -19,60 +13,5 @@ K = ["-k", f"not ({SKIP_OR})"]
 PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long", *K]
 
 
-def clean(path: Path, retries: int = 1) -> None:
-    time.sleep(1)
-    print("setting permissions...", flush=True)
-    failed = 0
-    for p in reversed([path, *sorted(path.rglob("*"))]):
-        print(f"... {p}")
-        try:
-            p.chmod(0o777)
-            if p.is_dir():
-                shutil.rmtree(p, ignore_errors=True)
-            elif p.is_file():
-                p.unlink()
-        except Exception as err:
-            print(f"!!! {err}", flush=True)
-            failed += 1
-    try:
-        if path.exists():
-            shutil.rmtree(path)
-    except Exception as err:
-        print("!!!", err, flush=True)
-        if retries:
-            print(f"... trying {retries} more time(s)", flush=True)
-            clean(path, retries - 1)
-    else:
-        return
-
-    shutil.rmtree(path, ignore_errors=True)
-
-
-def main():
-    print(">>>", *PYTEST, flush=True)
-    tmp = Path(tempfile.mkdtemp())
-    env = dict(os.environ)
-    env.update(PYTHONDONTWRITEBYTECODE="1")
-    env["TMP"] = env["TEMP"] = str(tmp / "TEMP")
-    shutil.copytree(Path("tests").resolve(), tmp / "tests")
-    proc = psutil.Popen([*PYTEST], cwd=str(tmp), env=env)
-    rc = proc.wait()
-    all_procs = [*proc.children(recursive=True), proc]
-    killed = []
-    for p in all_procs:
-        print(p, end="")
-        if not p.is_running():
-            print("... was not running", flush=True)
-            continue
-        killed += [p]
-        print("... killed", flush=True)
-    if killed:
-        psutil.wait_procs(killed)
-
-    clean(tmp, 3)
-
-    return rc
-
-
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(call(PYTEST))
